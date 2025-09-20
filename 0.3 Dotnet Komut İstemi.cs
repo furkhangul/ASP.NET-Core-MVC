d70a3465a8caace19a7a355c75dc7a48582d0442
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-20 13:27:58 +0300

    3

---
+++ 0.3 Dotnet Komut İstemi.cs
@@ -0,0 +1,14 @@
+//Dotnet Komut İstemi
+//Bilgisayarda uygun bir klasör oluşturun veya klasöre gidin.
+//Terminal veya Komut İstemi'ni açın.
+//Komut "dotnet" yazın ve Enter'a basın.
+//Bu çıktıda .Net SDK sürümünüzü ve diğer bilgileri görmeliyiz.
+//Optionslar için "dotnet --help" yazabilirsiniz.
+//Versiyon kontrolü için "dotnet --version" yazabilirim.
+//6 üzeri sürüm yüklü olduğundan emin olun.
+//SDK listesi için "dotnet --list-sdks" yazabilirim.
+//Runtime listesi için "dotnet --list-runtimes" yazabilirim.
+//Proje oluşturmak için ise:
+//"dotnet new console -o Notes" yazıyorum.
+//Eğer tam olarak anlayamazsak "dotnet new --help" yazarsam liseleri alabilirim.
+//"dotnet new list" yazarak tüm şablonları listeleyebilirim.
