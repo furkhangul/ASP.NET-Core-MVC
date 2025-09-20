516fc0efb41e2f95027246cc642f8baa6d64d4e0
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-20 14:33:49 +0300

    4

---
+++ 0.4 Çalışma Ortamını Düzenleme.cs
@@ -0,0 +1,15 @@
+//Çalışma Ortamını Düzenleme
+//Bu bölümde Visual Studionun kendimiz için nasıl düzenlediğini yazdım.
+//Notlarımızı ve kodlarımız yazacağımız proje dosyasına gidiyoruz.
+//Terminali açıp aşağıdaki komutu yazıyoruz.
+//"code ."
+//Bu komut Visual Studio Code programını açar.
+//Sol altta Manage ZComert menüsüne tıklıyoruz.
+//Buradan yeni profil oluşturuyoruz.
+//Editörde sıfırdan kendi bakışacımızı oluşturuyoruz.
+//Uzantılar bölümünde @installed yazıp yüklü uzantılarımızı görebiliriz.
+//Alt tarafta terminal bölümünde yeni terminal açabiliyoruz.
+//Bu bölümde PowerShell, Command Prompt, Git Bash gibi farklı kabuklar açabiliriz.
+//"dotnet new mcv -h" kodu ile ASP.NET Core MVC projesi oluşturmak için kullanılabilecek şablonları ve seçenekleri listeleyebiliriz.
+//"dotnet new mvc -n ProjeAdi" kodu ile "ProjeAdi" adında yeni bir ASP.NET Core MVC projesi oluşturabiliriz.
+
