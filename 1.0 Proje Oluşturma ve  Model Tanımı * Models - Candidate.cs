9dc4cd779b8b4903b404fda078b9f568e1a5803d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-21 15:31:24 +0300

    10

---
+++ 1.0 Proje Oluşturma ve  Model Tanımı * Models - Candidate.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Login.Models
+
+{
+    public class Candidate
+    {
+        public String email { get; set; } = string.Empty;
+        public String FirstName { get; set; } = string.Empty;
+        public String LastName { get; set; } = string.Empty;
+        public String FullName => $"{FirstName} {LastName.ToUpper()}";
+        public int Age { get; set; } = 0;
+        public String SelectedCourse { get; set; } = string.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate(){
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
