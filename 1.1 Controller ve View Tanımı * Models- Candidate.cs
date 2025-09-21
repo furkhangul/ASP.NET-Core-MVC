23c2ef133066066675a3f414d77290a3e768ccb8
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-21 16:10:32 +0300

    1

---
+++ 1.1 Controller ve View Tanımı * Models- Candidate.cs
@@ -0,0 +1,18 @@
+namespace Basic.Models
+
+{
+    public class Candidate
+    {
+        public String email { get; set; } = string.Empty;
+        public String FirstName { get; set; } = string.Empty;
+        public String LastName { get; set; } = string.Empty;
+        public String FullName => $"{FirstName} {LastName.ToUpper()}";
+        public int Age { get; set; } = 0;
+        public String SelectedCourse { get; set; } = string.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate(){
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
