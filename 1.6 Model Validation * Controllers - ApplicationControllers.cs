cc6a81015d616477844632e210ad4907e664fb5d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-23 13:15:22 +0300

    16

---
+++ 1.6 Model Validation * Controllers - ApplicationControllers.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Basic.Models;
+namespace Basic.Controllers
+{
+    public class Application : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var model = Repostory.Application;
+            return View(model);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //Doğrulama Mekanizması
+        [ValidateAntiForgeryToken]
+
+        //Formdan geldiğini belirtip Model bölümündeki Candidate Yapısına Aktardık
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repostory.Application.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already an application for you !");
+            }
+            if (ModelState.IsValid)
+            {
+                Repostory.Add(model);
+                return View("Feedback", model);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
