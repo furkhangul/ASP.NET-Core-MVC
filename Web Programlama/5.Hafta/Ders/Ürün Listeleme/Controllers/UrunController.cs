84495d30cba7fbf00042179b6b694e7fac9fb30a
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-10-23 12:28:48 +0300

    Add files via upload

---
+++ Web Programlama/5.Hafta/Ders/Ürün Listeleme/Controllers/UrunController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Deneme.Models;
+namespace Deneme.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Urun> urunler = new List<Urun>
+            {
+                new Urun(1, "Laptop", "Yüksek performanslı laptop", 7500.5),
+                new Urun(2, "Akıllı Telefon", "Son model akıllı telefon", 4500.6),
+                new Urun(3, "Tablet", "Taşınabilir tablet cihazı", 3000.5),
+                new Urun(4, "Akıllı Saat", "Fitness takip özellikli akıllı saat", 1500.8)
+            };
+
+            return View("Index", urunler);
+        }
+
+    }
+}
---
+++ Web Programlama/5.Hafta/Ders/Ürün Listeleme/Models/Urun.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+
+        public Urun()
+        {
+        }
+
+        public Urun(int id, string name, string description, double price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
