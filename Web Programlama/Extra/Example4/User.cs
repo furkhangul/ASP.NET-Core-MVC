83474ff997edc1fa4a238d9423d55a2c0da1b1d6
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-11-10 19:46:20 +0300

    Models - User.cs

---
+++ Web Programlama/Extra/Example4/User.cs
@@ -0,0 +1,22 @@
+namespace Basic.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+
+        public User()
+        { 
+        }
+
+        public User(int id, string name, string email, string phone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.Phone = phone;
+        }
+    }
+}
