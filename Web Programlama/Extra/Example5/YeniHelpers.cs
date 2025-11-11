d63b3751576fe98c0d6c719742760b80826824a3
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-11-11 10:15:32 +0300

    yen

---
+++ Web Programlama/Extra/Example5/YeniHelpers.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Basic.Helpers
+{
+    public static class YeniHelper
+    {
+        public static IHtmlContent YeniTextBox(this IHtmlHelper helpers, string name,string value)
+        {
+            return helpers.TextBox(name, value, new
+            {
+                @class = "form-control"
+            });
+        }
+        
+    }
+}
