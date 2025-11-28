e873206bb290fc7e44c8a23c94a146d6e628094a
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-11-29 01:27:05 +0300

    Add files via upload

---
+++ Web Programlama/9.Hafta/Velidation - MetaData/DataMetaData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class DataMetaData
+    {
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public string Email { get; set; }
+        [Length(4,18,ErrorMessage = "Geçerli Uzunluk Giriniz !")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public string Password { get; set; }
+    }
+}
