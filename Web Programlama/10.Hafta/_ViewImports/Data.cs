e5bf11350ae992fcd1dbe6d200d6c6d975e3224b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-12-02 17:38:40 +0300

    Add files via upload

---
+++ Web Programlama/10.Hafta/_ViewImports/Data.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basic.Models
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Data(int id, string name, string surname)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public Data()
+        {
+        }
+    }
+}
