cad29d13ef4e8f370d3aa427d9c21a819f245946
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-12-02 18:03:58 +0300

    Add files via upload

---
+++ Web Programlama/10.Hafta/Layout Kullanımı/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Basic.Models;
+namespace Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ekle(Data data)
+        {
+            ViewBag.User = data.Name + data.Surname;
+            return View(data);
+        }
+    }
+}
