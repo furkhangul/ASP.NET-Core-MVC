d8295c52e6b49072f5bf0b5563f46b0fd3a7172a
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-12-26 21:03:59 +0300

    Add files via upload

---
+++ Web Programlama/11.Hafta/ViewComponent/Models/Personel.cs
@@ -0,0 +1,20 @@
+﻿namespace Project.Models
+{
+    public class Personel
+    {
+        public string Ad { get; set; } = string.Empty;
+        public string Soyad { get; set; } = string.Empty;
+        public string Departman { get; set; } = string.Empty;
+        public Personel(string ad, string soyad, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+        }
+        public Personel()
+        {
+        }
+
+    }
+
+}
---
+++ Web Programlama/11.Hafta/ViewComponent/ViewCompanent/PersonellerViewCompanent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+
+namespace Project.ViewComponents
+{
+    public class PersonellerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<Personel> personel = new List<Personel>()
+            {
+                new Personel(){Ad="Ahmet",Soyad="Yılmaz",Departman="Yazılım"},
+                new Personel(){Ad="Mehmet",Soyad="Kara", Departman="Güvenlik"},
+                new Personel(){Ad="Ayşe",Soyad="Demir", Departman="Direktör"},
+                new Personel(){Ad="Fatma",Soyad="Şahin", Departman="Reklam"},
+            };
+
+            return View(personel);
+        }
+    }
+}
