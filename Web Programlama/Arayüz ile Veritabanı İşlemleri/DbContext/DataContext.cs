f682a9a6bd6e348f4acab2a4c7748ff3dbab3d00
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-01-20 17:43:46 +0300

    Add files via upload

---
+++ Web Programlama/Arayüz ile Veritabanı İşlemleri/DbContext/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FurOfTheWeak.Models;
+namespace FurOfTheWeak.DbContext
+{
+    public class UserDatum : Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=FurOfTheWeak;Trusted_Connection=True; TrustServerCertificate=True");
+        }
+
+        public DbSet<Data> User { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Data>(entity =>
+            {
+                entity.ToTable("Users");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Surname).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Birthday).IsRequired();
+                entity.Property(e => e.Password).IsRequired().HasMaxLength(100);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Web Programlama/Arayüz ile Veritabanı İşlemleri/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurOfTheWeak.Models
+{
+    public class Data
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        public string Password { get; set; } = string.Empty;
+    }
+}
